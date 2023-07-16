dc477e1b27bf3931c9c6c4781db515d7a65cda80
TesandriaP <130936656+TesandriaP@users.noreply.github.com>
2023-07-16 14:15:22 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -12,4 +12,14 @@ function App() {
   );
 }
 
+<footer>
+       <a href="https://github.com/TesandriaP/vanilla-weather-app"
+     target="_blank" rel="noopener">
+      Open-source code</a> 
+      by
+      <em>
+            Penn626
+      </em>
+    </footer>
+
 export default App;
