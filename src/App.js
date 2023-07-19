24f38b8dcf56f12827240cddcf9b25dcd3858bc4
TesandriaP <130936656+TesandriaP@users.noreply.github.com>
2023-07-19 18:00:24 -0400

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
-import Search from './Search';
+import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-        <h1>
-          Weather Search Engine
-        </h1>
-        <Search />
+      <h1>Weather Search Engine</h1>
+      <Search />
+      <footer>
+        <a
+          href="https://github.com/TesandriaP/vanilla-weather-app"
+          target="_blank"
+          rel="noopener"
+        >
+          Open-source code
+        </a>
+        by
+        <em>Penn626</em>
+      </footer>
     </div>
   );
 }
 
 export default App;
-<footer>
-       <a href="https://github.com/TesandriaP/vanilla-weather-app"
-     target="_blank" rel="noopener">
-      Open-source code</a> 
-      by
-      <em>
-            Penn626
-      </em>
-    </footer>
